94d8ebebdc892460548254bd3d589b2bf3e336c3
Andreas <atarta@htl-steyr.ac.at>
2022-11-15 15:35:08 +0100

    Initial commit

---
+++ src/main/java/htl/steyr/bestellungsaufnahme/BestellungsaufnahmeApplication.java
@@ -0,0 +1,13 @@
+package htl.steyr.bestellungsaufnahme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestellungsaufnahmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestellungsaufnahmeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/htl/steyr/bestellungsaufnahme/BestellungsaufnahmeApplicationTests.java
@@ -0,0 +1,13 @@
+package htl.steyr.bestellungsaufnahme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestellungsaufnahmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
